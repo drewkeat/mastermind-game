d02b295c59e3e19bfae2d97a7244b2661ed474fe
Drew Keat <thedrewkeat@gmail.com>
2021-10-19 22:33:52 -0600

    experiment with signup form and loading order

--- mastermind-client/src/index.js
+++ mastermind-client/src/index.js
@@ -1,1 +1,28 @@
 console.log('linked')
+
+const signupButton = document.getElementById("openSignup")
+
+signupButton.addEventListener("click", event => {
+    clearDoc();
+    document.body.innerHTML = 
+    `<div id="signup-form">
+        <form action="/users" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="userNameInput">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="userEmailInput">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="userPasswordInput">
+            <label for="password_confirmation">Confirm Password:</label>
+            <input type="password" name="password_confirmation" id="userPasswordConfirmation">
+            <div class="buttons">
+                <input type="submit" value="Create Account">
+                <input type="button" value="Login">
+            </div>
+        </form>
+    </div>`
+})
+
+function clearDoc() {
+    document.body.innerHTML=""
+}
