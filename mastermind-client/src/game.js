c051ca2feaf24d189574f3ab1f9772066abe7f1d
Drew Keat <thedrewkeat@gmail.com>
2021-10-31 13:16:21 -0600

    remove 'bindeEventListeners()' from Game.begin() to avoid duplicating listeners, rewrite Game: .generateCombo() .checkGuess() .checkAccuracy() .renderFeedback() .changeActiveRow() and displayCombo() methods)

--- mastermind-client/src/game.js
+++ mastermind-client/src/game.js
@@ -15,7 +15,6 @@ class Game {
 
     begin() {
         this.renderBoardState()
-        this.bindEventListeners()
         this.generateCombo()
     }
 
@@ -40,7 +39,7 @@ class Game {
     bindEventListeners() {
         const dropzones = document.querySelectorAll('[data-active-row] > .row >*')
         const draggables = document.querySelectorAll('[draggable]')
-        const guessButton = document.querySelectorAll('[data-active-row] >* button')
+        const guessButton = document.querySelector('[data-active-row] > .action-box > button')
 
         draggables.forEach(peg => 
             {
@@ -74,18 +73,83 @@ class Game {
             )
         })
 
-        guessButton.addEventListener('click', checkGuess())
+        guessButton.addEventListener('click', e => this.checkGuess())
     
     }
 
     generateCombo() {
+        const colors = [];
+        const pegs = document.querySelectorAll('.peg')
+        pegs.forEach(peg => colors.push(peg.classList[1]))
+        this.combo = []
+        for (let i=0; i < 4; i++) {
+            let key = colors[Math.floor(Math.random() * colors.length)]
+            this.combo.push(key)
+        }
     }
 
     checkGuess() {
+        const guessPegs = document.querySelectorAll('[data-active-row] > .row > .hole')
+        const guess = []
+        guessPegs.forEach(peg => guess.push(peg.classList[1]))
+        let accuracy = this.checkAccuracy(guess)
+        console.log(accuracy)
+        this.renderFeedback(accuracy)
+        this.changeActiveRow()
         }
 
+    checkAccuracy(guess) {
+        let rightPlace = 0
+        let rightColor = 0
+        let tempCombo = [...this.combo]
+        tempCombo.forEach((key, index) => {
+            if (guess[index] === key) {
+                rightPlace += 1;
+                guess[index] = 'x'
+                tempCombo[index] = 'o'
+            }
+        })
+
+        guess.forEach((color,index) => {
+            for (let i=0; i < tempCombo.length; i++) {
+                if (color === tempCombo[i]){
+                    rightColor += 1
+                    color = 'x'
+                    guess[index] = 'x'
+                    tempCombo[i] = 'o'
+                }
+            }
+        })
+        return [rightPlace, rightColor]
+    }
+
+    renderFeedback(accuracy) {
+        const feedbackHoles = document.querySelectorAll('[data-active-row] > .feedback-box >*')
+        const numRightPlace = accuracy[0]
+        const numRightColor = accuracy[1]
+        for (let i = 0; i < numRightPlace; i++) {
+            feedbackHoles[i].classList.add('right-place')
+        }
+        let startPlacePegs = numRightPlace
+        for (let i = 0; i < accuracy[1]; i++) {
+            feedbackHoles[startPlacePegs].classList.add('right-color')
+            startPlacePegs ++
+        }
+    }
+
+    changeActiveRow() {
+        // debugger
+        const currentRow = document.querySelector('[data-active-row]')
+        const currentRowNum = parseInt(currentRow.id.match(/guessRow(.*)/)[1])
+        const nextRow = document.querySelector(`#guessRow${currentRowNum - 1}`)
+        currentRow.toggleAttribute('data-active-row')
+        nextRow.toggleAttribute('data-active-row')
+        this.bindEventListeners()
+    }
 
     displayCombo() {
+        const solBoxes = document.querySelectorAll('.box')
+        this.combo.forEach((key,index) => solBoxes[index].className = `hole ${key}`)
     }
 
 }
